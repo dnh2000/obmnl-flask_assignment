209ace204313245f322ed76be3520d635b042bd8
dnh2000 <danielhildebrandt@live.com>
2024-02-26 12:41:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,18 @@ transactions = [
     {'id': 2, 'date': '2023-06-02', 'amount': -200},
     {'id': 3, 'date': '2023-06-03', 'amount': 300}
 ]
+filtered_transactions = []
+# Total Balance
+def get_total_balance(transactions):
+    balance = 0
+    for transaction in transactions:
+        balance += transaction['amount']
+    return balance
 # Read operation
 @app.route("/")
 def get_transactions():
-    return render_template("transactions.html", transactions=transactions)
+    balance = get_total_balance(transactions)
+    return render_template("transactions.html", transactions=transactions, balance=balance)
 # Create operation
 @app.route("/add", methods=["GET", "POST"])
 def add_transaction():
@@ -49,6 +57,26 @@ def delete_transaction(transaction_id):
             transactions.remove(transaction)
             break
     return redirect(url_for("get_transactions"))
+# Search operation
+@app.route("/search", methods= ["GET", "POST"])
+def search_transactions():
+    if request.method == 'POST':
+        minimum = float(request.form['min_amount'])
+        maximum = float(request.form['max_amount'])
+        for transaction in transactions:
+            if transaction['amount'] > minimum and transaction['amount'] < maximum:   
+                filtered_transactions.append(transaction)
+        balance = get_total_balance(filtered_transactions)
+        return render_template("transactions.html", transactions=filtered_transactions, balance=balance)
+    if request.method == 'GET':
+        return render_template("search.html", transactions=filtered_transactions)
+# Calculate operation
+@app.route("/balance")
+def total_balance():
+    balance = 0
+    for transaction in transactions:
+        balance += transaction['amount']
+    return f"Total Balance: {balance}"
 # Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
