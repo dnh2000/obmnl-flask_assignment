f714d785c90a24f1198f74e2b70985078d32d25c
dnh2000 <danielhildebrandt@live.com>
2024-02-26 09:18:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,27 @@
 # Instantiate Flask functionality
 
 # Sample data
-
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 # Read operation
-
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
 # Create operation
-
+@app.rout("/add", methods=["GET", "POST"])
+def add_transaction():
+    if request.method == 'POST':
+        transaction = {
+            'id': len(transactions) + 1,
+            'date': request.form['date'],
+            'amount': float(request.form['amount'])
+        }
+        transactions.append(transaction)
+        return redirect(url_for("get_transactions"))
+    return render_tempalte("form.html")
 # Update operation
 
 # Delete operation
